467d796ee2ec5088f7b3b687afcd1a2f1133a4c0
mariam <mariam@MYPROBOOK>
2023-03-16 16:05:19 +0400

    Add project files.

---
+++ Homework16.03/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace AbstractCLasses
+{
+    internal class Program
+    {
+        abstract class A
+        {
+            abstract public void Print();
+            public void Print1()
+            {
+                Console.WriteLine("Bye");
+            }
+        }
+        class B : A
+        {
+            public override void Print()
+            {
+                Console.WriteLine("Hello");
+            }
+        }
+        static void Main(string[] args)
+        {
+            A a = new B();
+            a.Print();
+            a.Print1();
+        }
+    }
+}
